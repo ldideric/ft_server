e879256c5a0cfb5a4c2fe4dc9781c01b3bc18e93
Lietze <lietze14@gmail.com>
2020-06-17 18:24:04 +0200

    wordpress updated

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@
 #    By: ldideric <ldideric@student.codam.nl>         +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/03/09 00:02:37 by nben-ezr      #+#    #+#                  #
-#    Updated: 2020/06/16 19:20:55 by ldideric      ########   odam.nl          #
+#    Updated: 2020/06/17 18:06:28 by ldideric      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,13 +18,25 @@ LABEL maintainer="Lietze Diderich ldideric"
 RUN apt-get update -y && \
 	apt-get upgrade -y
 
-# install SQL
-RUN apt-get install wget nginx mariadb-server mariadb-client php php-cli php-fpm \
-	php-cgi php-mysql php-mbstring openssl mcrypt php-mbstring php-pear -y && \
+# install requirements
+RUN apt-get install \
+	wget \
+	nginx \
+	mariadb-server \
+	mariadb-client \
+	php php-cli \
+	php-fpm \
+	php-cgi \
+	php-mysql \
+	php-mbstring \
+	openssl \
+	mcrypt \
+	php-mbstring \
+	php-pear -y && \
 	rm -rf /var/lib/apt/lists/*
 
+# generate SSL certificate
 RUN openssl req -x509 -nodes -days 365 -subj "/C=CA/ST=QC/O=Company, Inc./CN=localhost" -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt; 
-RUN cat /etc/ssl/private/nginx-selfsigned.key
 
 # copying sources to docker image
 ADD srcs/localhost /etc/nginx/sites-enabled/localhost
@@ -32,15 +44,18 @@ ADD srcs/localhost /etc/nginx/sites-enabled/localhost
 # connecting SQL to PHP
 #RUN pecl install sqlsrv pdo_sqlsrv
 
+# install wordpress
 RUN mkdir wordpress_temp
 COPY srcs/wordpress-5.3.2-nl_NL.tar.gz wordpress_temp
-
-# install wordpress
-RUN tar xvfz ./wordpress_temp/*
+RUN tar xvfz ./wordpress_temp/* && \
+	rm -rf wordpress_temp wordpress/wp-config-sample.php
+COPY srcs/wp-config.php wordpress/
 
 #80: http		443: https
 EXPOSE 80 443
 
+# start up nginx, mysql and php inside image
 CMD service nginx start && \
 	service mysql start && \
-	service php7.3-fpm start
+	service php7.3-fpm start \
+	tail -f /dev/null
---
+++ srcs/wp-config.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'ft_server' );
+
+/** MySQL database username */
+define( 'DB_USER', 'admin' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'password' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'd,|SeH<FG(:@kjz@)-?^bnEex]A_Br?<@}b5&sJc8zLzgEuUNy4DSp!FB[E+%>| ');
+define('SECURE_AUTH_KEY',  'Un?{1Vg?})IOuQ)qQ Jf{hyHyro+VWp5rSw6l`?sC=B}3AZ%N]M;4WGSi 2/jM|g');
+define('LOGGED_IN_KEY',    'g<FrHAwcXRV=QdEemy,C`poMDX~`zFL9`-?yU<k6XpU5OQz>i>-~USl Ib<m!K9f');
+define('NONCE_KEY',        'x&8%C4[nr_gk!x&|7yG)(3fj@|rfFMS`Aa_%#+LT0N+UWl)*eN|$O48~#U`PZ;=D');
+define('AUTH_SALT',        'FoJQ|aDcyeyO:L Y)vXKlH|Wptc1}?*s(4jim|{8[c,b<e<6X*vz|P+SEmB2J@Iz');
+define('SECURE_AUTH_SALT', '<QuJ#izm]_X~f81m.3hR9@UL}E/?$L[h,IYyQ7Q) e~.Bf/!O]@`sem@MjvuCKs9');
+define('LOGGED_IN_SALT',   '5vb1z/QQC)IDiRr)~3tQ`f)K7@.V:zq-{Uu6rA{G9wZ8?4sR.`=~-U[7nqYc>.-W');
+define('NONCE_SALT',       'K#J[`]ZnR>C *Wq93Y$@M~~6Mh5acgG-x S|:^m,dPkmw$`$+<i-M`=]q$@k-r#/');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
